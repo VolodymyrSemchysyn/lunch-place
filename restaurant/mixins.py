ec8633ab85c61c4c5c94df8744d642f94b35e995
Volodymyr <uuforreasonsunknownuu@gmail.com>
2025-02-14 08:41:50 +0200

    fix: changed mixin logic

--- restaurant/mixins.py
+++ restaurant/mixins.py
@@ -2,11 +2,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
+
 class VersionedAPIView(APIView):
     SUPPORTED_VERSIONS = ["1.0", "2.0"]
+    DEFAULT_VERSION = "1.0"
 
     def dispatch(self, request, *args, **kwargs):
-        build_version = request.headers.get("Build-Version")
-        if build_version and build_version not in self.SUPPORTED_VERSIONS:
+        build_version = request.headers.get("Build-Version", self.DEFAULT_VERSION)
+        if build_version not in self.SUPPORTED_VERSIONS:
             return Response({"error": "Unsupported version"}, status=status.HTTP_400_BAD_REQUEST)
         return super().dispatch(request, *args, **kwargs)
